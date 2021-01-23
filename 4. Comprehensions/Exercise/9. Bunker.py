18d79e5143c324248f29c241f77daf06fd20defb
Borislav-source <71929652+Borislav-source@users.noreply.github.com>
2021-01-23 15:38:06 +0200

    My solutions

--- 4. Comprehensions/Exercise/9. Bunker.py
+++ 4. Comprehensions/Exercise/9. Bunker.py
@@ -1,19 +1,16 @@
-items_categories = input().split(', ')
+categories = {category: [] for category in input().split(', ')}
 relations = int(input())
-my_dict = {}
 the_sum_of_quantity = 0
 the_sum_of_quality = 0
+
 for _ in range(relations):
-    data = input()
-    category, item, rest = data.split(' - ')
+    category, item, rest = input().split(' - ')
     quantity, quality = rest.split(';')
-    if category not in my_dict:
-        my_dict[category] = []
-    my_dict[category].append(item)
+    categories[category].append(item)
     the_sum_of_quantity += int(quantity.split(':')[1])
-    the_sum_of_quality += float(quality.split(':')[1])
+    the_sum_of_quality += int(quality.split(':')[1])
 
 
 print(f'''Count of items: {the_sum_of_quantity}
-Average quality: {the_sum_of_quality / len(my_dict):.2f}''')
-[print(f'{cat} -> {", ".join(my_dict[cat])}') for cat in items_categories]
+Average quality: {the_sum_of_quality / len(categories):.2f}''')
+[print(f'{cat} -> {", ".join(item)}') for cat, item in categories.items()]
