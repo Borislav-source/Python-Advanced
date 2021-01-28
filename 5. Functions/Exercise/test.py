0605fd12fc4ecf396bf23bb8989beb86746e9672
Borislav-source <tsv.borislav@gmail.com>
2021-01-28 20:44:29 +0200

    test

--- 5. Functions/Exercise/test.py
+++
@@ -1,8 +0,0 @@
-def palindrome(word, i):
-    if len(word) < 2 or word[0] == word[-1] and palindrome(word[1:-1], i):
-        return f"{word} is a palindrome"
-    else:
-        return f"{word} is not a palindrome"
-
-print(palindrome("abcba", 0))
-print(palindrome("petvp", 0))
