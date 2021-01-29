eae4896b39226b3782ae755ebb2bc5e72f5f05bc
Borislav-source <tsv.borislav@gmail.com>
2021-01-29 17:16:26 +0200

    Changes

---
+++ 5. Functions/Lab/test.py
@@ -0,0 +1,18 @@
+import os
+# try:
+#     file = open('text2.txt', 'r')
+# except:
+#     file = open('text2.txt', 'w')
+# file.close()
+os.remove('text2.txt')
+
+
+# line = "-I was quick to judge him, but it wasn't his fault."
+# line = line.split()
+# for el in range(len(line)):
+#     for ch in range(len(line[el])):
+#         if line[el][ch] in ['-', ',', '.', '!', '?']:
+#             line[el] = line[el][:ch] + '@' + line[el][ch + 1:]
+# print(line)
+# for el in range(1, len(line) + 1):
+#     print(line[-el], end=' ')
