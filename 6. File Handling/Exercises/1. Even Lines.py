a160ab31585cb32ee0e759f4c90617362b9d3dd3
Borislav-source <tsv.borislav@gmail.com>
2021-01-31 19:19:24 +0200

    Changes

--- 6. File Handling/Exercises/1. Even Lines.py
+++ 6. File Handling/Exercises/1. Even Lines.py
@@ -1,9 +1,10 @@
+import string
 file = open('text.txt', 'r')
 for line in file:
     line = line.split()
     for el in range(len(line)):
         for ch in range(len(line[el])):
-            if line[el][ch] in ['-', ',', '.', '!', '?']:
+            if line[el][ch] in string.punctuation:
                 line[el] = line[el][:ch] + '@' + line[el][ch + 1:]
     print(*list(reversed(line)))
 file.close()
